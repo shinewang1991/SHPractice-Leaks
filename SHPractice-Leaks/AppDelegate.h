4926a3b45c2f3fc972f5c3329f20daf7124a8dea
Eirc <eric.wang@qiffa.com>
2019-06-24 11:56:43 +0800

    Initial Commit

---
+++ SHPractice-Leaks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHPractice-Leaks
+//
+//  Created by Eric on 2019/6/24.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHPractice-Leaks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHPractice-Leaks
+//
+//  Created by Eric on 2019/6/24.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
